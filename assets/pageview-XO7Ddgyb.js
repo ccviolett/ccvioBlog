e392b7dedf561fd80a59a9838c524b35dc55017c
ccviolett <3340751717@qq.com>
2023-12-15 04:23:22 +0000

    Deploying to gh-pages from @ ccviolett/ccvioBlog@66bdbee9e18387171765b7af5af99fdc7ae1e91e 🚀

--- assets/pageview-XO7Ddgyb.js
+++
@@ -1,7 +0,0 @@
-import{a5 as t}from"./app-MPtnLaZt.js";var a={provider:"Waline",dark:'html[data-theme="dark"]',serverURL:"https://waline-comment.vuejs.press"};const i=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-MPtnLaZt.js").then(r=>r.af),__vite__mapDeps([]));return e({serverURL:a.serverURL})}catch{console.error("@waline/client is not installed!");return}};export{i as updatePageview};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
